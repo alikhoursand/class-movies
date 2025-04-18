7d1405f559c0fecb278ecea1893eef565f8554e5
alikhoursand <ali.khoursand@gmail.com>
2025-04-18 15:08:47 +0330

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="navbar bg-base-100 max-w-screen-xl mx-auto">
+        <div class="navbar-start">
+            <div class="dropdown">
+                <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h8m-8 6h16" />
+                    </svg>
+                </div>
+                <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <li><a>Item 1</a></li>
+                    <li>
+                        <a>Parent</a>
+                        <ul class="p-2">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            </div>
+            <a class="w-[175px]">
+                <img src="/public/logo-dark.png" alt="">
+            </a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+            <ul class="menu menu-horizontal px-1">
+                <li>
+                    <details>
+                        <summary>خانه</summary>
+                        <ul class="p-2">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </details>
+                </li>
+                <li>
+                    <details>
+                        <summary>ژانر</summary>
+                        <ul class="p-2">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+        </div>
+        <div class="navbar-end">
+            <button class="btn btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                    stroke="currentColor" class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+                </svg>
+
+                ثبت نام
+            </button>
+        </div>
+    </div>
+</template>
---
+++ src/components/Slider.vue
@@ -0,0 +1,51 @@
+<template>
+    <swiper :navigation="true" :modules="modules" class="rounded-box mySwiper h-[450px] object-cover object-center">
+        <swiper-slide>
+            <div class="relative">
+                <div class="absolute w-full h-full bg-neutral opacity-30"></div>
+                <img src="/public/slider-1.jpg" class="" alt="">
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="relative">
+                <div class="absolute w-full h-full bg-neutral opacity-30"></div>
+                <img src="/public/slider-2.jpg" class="" alt="">
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div class="relative">
+                <div class="absolute w-full h-full bg-neutral opacity-30"></div>
+                <img src="/public/slider-3.jpg" class="" alt="">
+            </div>
+        </swiper-slide>
+
+    </swiper>
+</template>
+
+
+
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        return {
+            modules: [Navigation],
+        };
+    },
+};
+</script>
